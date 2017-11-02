c6b56b8c021e6422ca6671efd12bec48245409d4
Anurag Singh <anurag.singh@zycus.com>
2017-11-02 11:48:50 +0530

    added Controller and model for bus and tickets

--- application/views/admin/layout/sideBar.php
+++ application/views/admin/layout/sideBar.php
@@ -12,25 +12,6 @@
             <!-- Optionally, you can add icons to the links -->
             <li><a href="<?php echo base_url() ;?>admin/dashboard/dashboard"><i class="fa fa-link"></i> <span>Dashbaord</span></a></li>
 			
-            <li><a href="<?php echo base_url() ;?>admin/newsletters/newsletters"><i class="fa fa-link"></i> <span>Newsletters</span></a></li>
-            
-            <li class="treeview">
-              <a href="#"><i class="fa fa-link"></i> <span>Recipes Management</span> <i class="fa fa-angle-left pull-right"></i></a>
-              <ul class="treeview-menu">
-                <li><a href="<?php echo base_url() ;?>admin/recipes/recipes/addRecipe">Create Recipes</a></li>
-                <li><a href="<?php echo base_url() ;?>admin/recipes/recipes">Manage Recipes</a></li>
-                <li><a href="<?php echo base_url() ;?>admin/recipes/recipes/recipeCategory">Recipes Category</a></li>
-                <li><a href="<?php echo base_url() ;?>admin/reviews/reviews">Manage Reviews</a></li>
-              </ul>
-            </li>
-            <li class="treeview">
-              <a href="#"><i class="fa fa-link"></i> <span>Blogs</span> <i class="fa fa-angle-left pull-right"></i></a>
-              <ul class="treeview-menu">
-                <li><a href="<?php echo base_url() ;?>/admin/blogs/blogs/addBlog">Create Blogs</a></li>
-                <li><a href="<?php echo base_url() ;?>/admin/blogs/blogs">Manage Blogs</a></li>
-                <li><a href="<?php echo base_url() ;?>/admin/blogs/blogs/blogCategory">Blogs Category</a></li>
-              </ul>
-            </li>
 			<?php if($this->session->userdata['logged_in']['role']==1) {
 				echo' <li class="treeview">
               <a href="#"><i class="fa fa-link"></i> <span>Admin User Management</span> <i class="fa fa-angle-left pull-right"></i></a>
