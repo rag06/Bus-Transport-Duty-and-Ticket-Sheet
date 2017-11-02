24fedec09971d26fac6a2e5c71ff349ec78ec9cc
Anurag Singh <anurag.singh@zycus.com>
2017-11-02 15:06:32 +0530

    view for ticekts listing

---
+++ application/views/admin/tickets/index.php
@@ -0,0 +1,108 @@
+<?php $this->load->view('admin/layout/header.php');?>
+  <body class="hold-transition skin-blue sidebar-mini">
+    <div class="wrapper">
+	<?php $this->load->view('admin/layout/mainHeader.php');?>
+	<?php $this->load->view('admin/layout/sideBar.php');?>
+      
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+          <h1>
+            Tickets List
+            <small>Manage Your Tickets</small>
+          </h1>
+          <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+            <li class="active"><a href="#">Tickets List</a></li>
+          </ol>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+				 <div class="box box-primary">
+						<div class="box-header with-border">
+						  <h3 class="box-title">Tickets List</h3>
+						</div><!-- /.box-header -->
+						  <div class="box-body">
+							<table id="webpagesList" class="table table-bordered table-hover">
+								<thead>
+								<tr>
+								  <th style="width: 10px">#</th>
+								  <th>Price</th>
+								  <th>Extra Price</th>
+								  <th>Ticket Type</th>
+								  <th>Status</th>
+								  <th>Actions</th>
+								</tr>
+								</thead>
+								<tbody>
+								<?php 
+								$i=1;
+									foreach($result['result'] as $row){
+										?>
+									<tr>
+									  <td><?php echo $i;?>.</td>
+									  <td><?php echo 'Rs. '.$row->tickets_Price;?></td>
+									  <td><?php echo 'Rs. '.$row->tickets_ExtraPrice;?></td>
+									  <td>
+									  <?php if($row->tickets_Type==1){
+												echo'Concessional';
+											}else{
+													echo'General';
+											}
+										?>
+									</td>
+									<td>
+									  <?php if($row->tickets_Status==1){
+												echo'<span class="badge bg-green"> Active </span>';
+											}else{
+													echo'<span class="badge bg-warning">InActive</span>';
+											}
+										?>
+									</td>
+									  <td>
+										<a href="<?php echo base_url() ;?>/admin/tickets/tickets/editTicket/<?php echo $row->tickets_Id;?>" class="btn  btn-info btn-sm" >Edit</a>
+										
+										<button onclick="deleteTicket(<?php echo $row->tickets_Id;?>)" class="btn  btn-danger btn-sm">Delete</button>
+									  </td>
+									</tr>
+										<?php $i++;}?>
+								</tbody>
+								
+							  </table>
+						  </div><!-- /.box-body -->
+					</div><!--box end-->
+			</section><!-- /.content -->
+      </div><!-- /.content-wrapper -->
+		 <div class="modal fade modal-danger" id="deleteTicket" >
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title">Confirm Delete</h4>
+                  </div>
+                  <div class="modal-body">
+                    <p>Are You Sure You Want to Delete ??</p>
+					<form action="<?php echo base_url() ;?>/admin/tickets/tickets/deleteTicket" method="post">
+						<input type="hidden" name="ticketId" id="ticketId"/>
+						<input type="submit" class="btn btn-outline" value="Yes">
+					</form>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-outline pull-right" data-dismiss="modal">Close</button>
+					
+                  </div>
+                </div><!-- /.modal-content -->
+              </div><!-- /.modal-dialog -->
+            </div><!-- /.modal -->
+     <?php $this->load->view('admin/layout/footer.php');?>
+	 
+	<!-- admin manage page-->
+	<script>
+		function deleteTicket(id){
+			$('#ticketId').val(id);
+			$("#deleteTicket").modal();
+			
+		}
+	</script>
