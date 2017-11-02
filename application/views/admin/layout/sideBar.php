ddfe63a0a5a2e0696b405e0b1e738150539dae77
Anurag Singh <anurag.singh@zycus.com>
2017-11-02 15:03:21 +0530

    view for ticekts listing

--- application/views/admin/layout/sideBar.php
+++ application/views/admin/layout/sideBar.php
@@ -1,27 +1,48 @@
 <aside class="main-sidebar">
-
         <!-- sidebar: style can be found in sidebar.less -->
         <section class="sidebar">
-
-
-        
-
           <!-- Sidebar Menu -->
           <ul class="sidebar-menu">
             <li class="header">Navigation</li>
             <!-- Optionally, you can add icons to the links -->
-            <li><a href="<?php echo base_url() ;?>admin/dashboard/dashboard"><i class="fa fa-link"></i> <span>Dashbaord</span></a></li>
+            <li><a href="<?php echo base_url() ;?>admin/dashboard/dashboard"><i class="fa fa-link"></i> <span>Dashboard</span></a></li>
+			<li class="treeview">
+              <a href="#"><i class="fa fa-link"></i> <span>Tickets Management</span> <i class="fa fa-angle-left pull-right"></i></a>
+              <ul class="treeview-menu">
+                <li><a href="<?php echo base_url()?>admin/tickets/tickets/addTicket">Create Tickets</a></li>
+                <li><a href="<?php echo base_url()?>admin/tickets/tickets">Manage Tickets</a></li>
+                <li><a href="<?php echo base_url()?>admin/tickets/Ticket_Register/addTicketRegister">Add Entry in Ticket Register</a></li>
+                <li><a href="<?php echo base_url()?>admin/tickets/Ticket_Register">Manage Ticket Register</a></li>
+              </ul>
+            </li>
+			<li class="treeview">
+              <a href="#"><i class="fa fa-link"></i> <span>Bus Management</span> <i class="fa fa-angle-left pull-right"></i></a>
+              <ul class="treeview-menu">
+                <li><a href="<?php echo base_url()?>admin/bus/bus/addBusRoute">Add Bus Route</a></li>
+                <li><a href="<?php echo base_url()?>admin/bus/bus">Manage Bus Route</a></li>
+                <li><a href="<?php echo base_url()?>admin/bus/Bus_Timing/addBusTiming">Add Bus Timing</a></li>
+                <li><a href="<?php echo base_url()?>admin/bus/Bus_Timing">Manage Bus Timing</a></li>
+              </ul>
+            </li>
+			<li class="treeview">
+              <a href="#"><i class="fa fa-link"></i> <span>Employees Management</span> <i class="fa fa-angle-left pull-right"></i></a>
+              <ul class="treeview-menu">
+                <li><a href="<?php echo base_url()?>admin/employees/employees/addEmployee">Add Employee</a></li>
+                <li><a href="<?php echo base_url()?>admin/employees/employees">Manage Employee</a></li>
+                <li><a href="<?php echo base_url()?>admin/employees/Employee_Tickets/addTicketEmployee">Assign Tickets </a></li>
+              </ul>
+            </li>
 			
 			<?php if($this->session->userdata['logged_in']['role']==1) {
 				echo' <li class="treeview">
-              <a href="#"><i class="fa fa-link"></i> <span>Admin User Management</span> <i class="fa fa-angle-left pull-right"></i></a>
-              <ul class="treeview-menu">
-                <li><a href="'.base_url() .'admin/admin/users/addUser">Create Admin</a></li>
-                <li><a href="'.base_url().'admin/admin/users">Manage Admin</a></li>
-              </ul>
-            </li>';
-				
-				
+						  <a href="#"><i class="fa fa-link"></i> <span>Admin User Management</span> <i class="fa fa-angle-left pull-right"></i></a>
+						  <ul class="treeview-menu">
+							<li><a href="'.base_url() .'admin/admin/users/addUser">Create Admin</a></li>
+							<li><a href="'.base_url().'admin/admin/users">Manage Admin</a></li>
+						  </ul>
+						</li>';
+					
+					
 			}?>
            
             <li><a href="<?php echo base_url() ;?>admin/files/files"><i class="fa fa-link"></i> <span>Files Explorer</span></a></li>
