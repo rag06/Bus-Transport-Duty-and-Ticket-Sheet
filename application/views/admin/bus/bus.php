9aac1e3a99da188e62b271483a6d2500d8c35572
Anurag Singh <singhanuragv@gmail.com>
2017-11-19 07:51:00 +0530

    git corrections in waybill and add bus list management

---
+++ application/views/admin/bus/bus.php
@@ -0,0 +1,115 @@
+<?php $this->load->view('admin/layout/header.php');?>
+  <body class="hold-transition skin-blue sidebar-mini">
+    <div class="wrapper">
+	<?php $this->load->view('admin/layout/mainHeader.php');?>
+	<?php $this->load->view('admin/layout/sideBar.php');?>
+      
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+          <h1>
+            Bus  List
+            <small>Manage Your Bus </small>
+          </h1>
+          <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+            <li class="active"><a href="#">Bus  List</a></li>
+          </ol>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+				 <div class="box box-primary">
+						<div class="box-header with-border">
+						  <h3 class="box-title">Bus  List
+										<button onclick="addBus();" class="btn admin-btns btn-success btn-sm">Add</button>
+						</h3>
+						</div><!-- /.box-header -->
+						  <div class="box-body">
+							<table id="webpagesList" class="table table-bordered table-hover">
+								<thead>
+								<tr>
+								  <th style="width: 10px">#</th>
+								  <th>Number</th>
+								  <th>Actions</th>
+								</tr>
+								</thead>
+								<tbody>
+								<?php 
+								$i=1;
+									foreach($result['result'] as $row){
+										?>
+									<tr>
+									  <td><?php echo $i;?>.</td>
+									  <td><?php echo $row->bus_number;?></td>
+									  <td>
+										
+										<button onclick="deleteBus('<?php echo $row->bus_number;?>')" class="btn admin-btns btn-danger btn-sm">Delete</button>
+									  </td>
+									</tr>
+										<?php $i++;}?>
+								</tbody>
+								
+							  </table>
+						  </div><!-- /.box-body -->
+					</div><!--box end-->
+			</section><!-- /.content -->
+      </div><!-- /.content-wrapper -->
+		 <div class="modal fade modal-danger" id="deleteBus" >
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title">Confirm Delete</h4>
+                  </div>
+                  <div class="modal-body">
+                    <p>Are You Sure You Want to Delete ??</p>
+					<form action="<?php echo base_url() ;?>admin/bus/bus/deleteBus" method="post">
+						<input type="hidden" name="busNo" id="busNo"/>
+						<input type="submit" class="btn btn-outline" value="Yes">
+					</form>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-outline pull-right" data-dismiss="modal">Close</button>
+					
+                  </div>
+                </div><!-- /.modal-content -->
+              </div><!-- /.modal-dialog -->
+            </div><!-- /.modal -->
+		 <div class="modal fade modal-default" id="addBus" >
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title">Add Bus</h4>
+                  </div>
+                  <div class="modal-body">
+                    <p>Enter Bus Number</p>
+					<form action="<?php echo base_url() ;?>admin/bus/bus/insertBus" name="addBusForm" method="post">
+						<input type="text" class="form-control" name="busNo" id="busNo" placeholder="Enter Bus Number"  required/>
+						<br/>
+						<input type="submit" class="btn btn-info insertBus" value="Submit">
+					</form>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-warning pull-right" data-dismiss="modal">Close</button>
+					
+                  </div>
+                </div><!-- /.modal-content -->
+              </div><!-- /.modal-dialog -->
+            </div><!-- /.modal -->
+     <?php $this->load->view('admin/layout/footer.php');?>
+	 
+	<!-- admin manage page-->
+	<script>
+		function deleteBus(id){
+			$('#busNo').val(id);
+			$("#deleteBus").modal();
+			
+		}
+		function addBus(){
+			$("#addBus").modal();
+			
+		}
+	</script>
